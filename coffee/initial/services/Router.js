7c34f1029b365fe6c275b3b12381a703c8f099ef
Derek Belloni <derekbelloni10@gmail.com>
2024-11-13 21:39:04 -0700

    implemented startViewTransition, played around with animation duration

--- coffee/initial/services/Router.js
+++ coffee/initial/services/Router.js
@@ -14,7 +14,7 @@ const Router = {
         // Process initial URL   
         Router.go(location.pathname);
     },    
-    go: (route, addToHistory=true) => {
+    go: async (route, addToHistory=true) => {
         if (addToHistory) {
             history.pushState({ route }, '', route);
         }
@@ -24,6 +24,8 @@ const Router = {
                 pageElement = document.createElement("menu-page");
                 break;
             case "/order":
+                // Lazy Load Pattern
+                await import("../components/OrderPage.js");
                 pageElement = document.createElement("order-page");
                 break;
             default:
@@ -34,15 +36,21 @@ const Router = {
                 break;   
         }
         if (pageElement) {
-            // get current page element            
-            let currentPage = document.querySelector("main").firstElementChild; 
-            if (currentPage) {
-                currentPage.remove();
-                document.querySelector("main").appendChild(pageElement);
-            } else {
-                document.querySelector("main").appendChild(pageElement);
+            const changePage = function() {
+                // get current page element            
+                let currentPage = document.querySelector("main").firstElementChild; 
+                if (currentPage) {
+                    currentPage.remove();
+                    document.querySelector("main").appendChild(pageElement);
+                } else {
+                    document.querySelector("main").appendChild(pageElement);
+                }
             }
-
+            if (document.startViewTransition) {
+                document.startViewTransition(() => changePage());
+            } else {
+                changePage();
+            }
         }
 
         window.scrollX = 0;
